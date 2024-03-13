5dca750663ad720780d74ce8a4301e266325f963
Anton <danilovanton724@gmail.com>
2024-03-13 15:44:57 +0100

    Initial Commit

---
+++ Whack Pliko/Whack_PlikoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whack_PlikoApp.swift
+//  Whack Pliko
+//
+//  Created by Anton on 13/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Whack_PlikoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
